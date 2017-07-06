91c62c86b4397882f58da92168b8ce248d258816
Alex-Cook4 <cooka@us.ibm.com>
2017-07-06 11:02:10 +0100

    Revert "Major overhaul to the Call Center solution"
    This reverts commit 9781ac269634541878361552548b44abc800e8d1.

--- applications/CallCenter/com.ibm.streamsx.speech2text.callcenter.speechprocessing/README.md
+++ applications/CallCenter/com.ibm.streamsx.speech2text.callcenter.speechprocessing/README.md
@@ -2,25 +2,21 @@ This set of jobs is for processing live RTP packets and transcribing the audio i
 
 =====================================
 
-Job descriptions (associated parameter descriptions): 
+Job descriptions: 
 
-**PacketHander** - Gets RTP traffic from the Network Tap application using a TCPSource. 
+**setup** - Gets RTP traffic from the Network Tap application using a TCPSource. 
 The RTP packets are then filtered for just audio and sent onwards using Exports. 
 The packets are then passes through a ControlFlow composite that directs them to the WatsonS2T
 engine that is designated for transcribing that audio stream or designates an open WatsonS2T engine
 to begin processing the stream. If the stream isn't being processed, and no WatsonS2T engines are 
 available, the packet will be dropped. 
 
-**WatsonSpeech2TextX** - Each of the Watson jobs are designed to process incoming RAW audio streams on a single host. 
-The number of engines and the number of hosts to use can be configured at submit-time using the ./submitJob.sh script.
-Or by using submit-time parameters for individual job submission. 
+**watsonX** - Each of the Watson jobs are designed to process incoming RAW audio streams on a single host. 
+The number of engines and the number of hosts to use can be configured in the Makefile. Typically, for 
+live processing you can have 2 WatsonS2T engines per core. 
 
-**DataCenterSink** - This job takes merges the data from all of the watsonX jobs, and sends them out via a 
+**outdata** - This job takes merges the data from all of the watsonX jobs, and sends them out via a 
 TCPSink. 
-
-**WavGenerator** - This job is not considered part of the "core group" and as such is not submitted with the "all" or "callcenter" options 
-on the submitJob.sh script. This job allows for saving the calls processed to .wav files for later playback or processing. This 
-application requires an installation of FFMPEG on the system being deployed to. 
 
 =====================================
 
@@ -28,44 +24,14 @@ Environment:
 
 1. Make sure that version 3.0.5 of the com.ibm.streamsx.network toolkit is available in your $STREAMS_SPLPATH, 
 along with the com.ibm.streams.speech2text toolkit. 
-	export STREAMS_SPLPATH=/path/to/network/toolkit:/path/to/speech2text/toolkit:$STREAMS_SPLPATH
-2. Refer to the Configuring your Domain and Instance README at the root of the CallCenter application directory 
-for domain and instance setup. 
+	export STREAMS_SPLPATH=/path/to/network/toolkit:/path/to/speech2text/toolkit$STREAMS_SPLPATH
 
 =====================================
 
 To compile this application run 
 
-	$ make  
+	$ make all 
 
 =====================================
 
-Submit the application - Use the submitJob.sh script for ease of deployment, or see the submit_example.sh for an example of submitjob commands.
-
-Example commands and descriptions:
- 
- Submit packethandler,watson1, watson2, watson3, and datacentersink -- watson1 with 1 speech2text engines running, watson2 with 2 speech2text engines running,
- watson3 with 3 speech2text engines running
- 	./submitJob.sh -j speechprocessing --numWatsonHosts 3 --watsonEngineCountList [1,2,3] 
- 
- Submit just packethandler, but configured for connecting to watson1 with 1 speech2text engine running, watson2 with 2 speech2text engines running,
- watson3 with 3 speech2text engines running
- 	./submitJob.sh -j packethandler --numWatsonHosts 3 --watsonEngineCountList [1,2,3]
- 
- Submit watson jobs: -- watson1 with 1 speech2text engines running, watson2 with 2 speech2text engines running,
- watson3 with 3 speech2text engines running
- 	./submitJob.sh -j watson --numWatsonHosts 3 --watsonEngineCountList [1,2,3]
- 
- Submit datacentersink -- no dependencies for this one. 
- 	./submitJob.sh -j datacentersink 
- 
- Submit wavgen -- no dependencies for this one. 
- 	./submitJob.sh -j wavgen
- 
- ===========================================================
- 
- ## Host Placement
- 
- Each of the jobs can be placed on a host of your choice by tagging that host with the tag 
- corresponding to the job. The tags can by modified without recompiling by modifying the 
- "targetTagSet" attribute in the etc/<job-name>JobConfig.json file. 
+Submit the application - See the submit_example.sh for an example of submitjob commands.
--- applications/CallCenter/com.ibm.streamsx.speech2text.callcenter.speechprocessing/submitJob.sh
+++ applications/CallCenter/com.ibm.streamsx.speech2text.callcenter.speechprocessing/submitJob.sh
@@ -3,184 +3,91 @@
 STREAMS_DEFAULT_DOMAIN=$STREAMS_DOMAIN_ID
 STREAMS_DEFAULT_IID=$STREAMS_INSTANCE_ID
 
-PACKETHANDLER_SAB_FILE=output/packethandler/com.ibm.streamsx.speech2text.callcenter.speechprocessing.packethandler.PacketHandler.sab
-WATSON_SAB_FILE=output/watsonspeech2text/com.ibm.streamsx.speech2text.callcenter.speechprocessing.watsons2t.WatsonSpeech2Text.sab
-DATACENTERSINK_SAB_FILE=output/datacentersink/com.ibm.streamsx.speech2text.callcenter.speechprocessing.datacentersink.DataCenterSink.sab
-WAV_SAB_FILE=output/wavgen/com.ibm.streamsx.speech2text.callcenter.speechprocessing.wavgen.WavGenerator.sab
+SETUP_SAB_FILE=output/setup/com.ibm.streamsx.speech2text.speechprocessing.setup.Main.sab
+WATSON1_SAB_FILE=output/watson_1/com.ibm.streamsx.speech2text.speechprocessing.Watson.sab
+TEST_SAB_FILE=output/test/com.ibm.streamsx.speech2text.speechprocessing.test.Main.sab
+WAV_SAB_FILE=output/wavgen/com.ibm.streamsx.speech2text.speechprocessing.wavgen.WavGenerator.sab
 
 declare -A sabmap
-sabmap[packethandler]=$PACKETHANDLER_SAB_FILE
-sabmap[watson]=$WATSON_SAB_FILE
-sabmap[datacentersink]=$DATACENTERSINK_SAB_FILE
+sabmap[setup]=$SETUP_SAB_FILE
+sabmap[watson1]=$WATSON1_SAB_FILE
+sabmap[test]=$TEST_SAB_FILE
 sabmap[wavgen]=$WAV_SAB_FILE
 
-declare -A jobnamemap
-jobnamemap[packethandler]=PacketHandler
-jobnamemap[watson]=WatsonXXX
-jobnamemap[datacentersink]=DataCenterSink
-jobnamemap[wavgen]=WavGenerator
 
 if [ "$#" -le 0 ] ; then
-	echo "Must specify one of -j|--job [packethandler,watson,datacentersink,wavgen]"
-	echo "Example commands: ./submitJob.sh -j packethandler --numWatsonHosts 3 --watsonEngineCountList [1,2,3]"
-	echo " ./submitJob.sh -j watson --numWatsonHosts 3 --watsonEngineCountList [1,2,3]"
-	echo "./submitJob.sh -j datacentersink "
-	echo "./submitJob.sh -j wavgen "
+	echo "must specify -h or -s [setup,watson1,test,wavgen]"]
 	exit
 fi
 
-if [[ $# -lt 2 ]]
-then
-	echo "Specify one of -j|--job [packethandler,watson,datacentersink,wavgen,speechprocessing,all]"
-    echo "Example commands:"
-    echo " ./submitJob.sh -j speechprocessing --numWatsonHosts 3 --watsonEngineCountList [1,2,3] # submit packethandler,watson,and datacentersink"
-    echo " ./submitJob.sh -j packethandler --numWatsonHosts 3 --watsonEngineCountList [1,2,3]"
-	echo " ./submitJob.sh -j watson --numWatsonHosts 3 --watsonEngineCountList [1,2,3]"
-	echo " ./submitJob.sh -j datacentersink "
-	echo " ./submitJob.sh -j wavgen "
-    exit
-fi
+while getopts ":hs:" optname
+  do
+    case "$optname" in
+      "h")
+        echo "Specify one of -s [setup,watson1,test,wavgen]"
+        exit
+	;;
+      "s")
+	JOB=$OPTARG
+        ;;
+      "?")
+        echo "Unknown option $OPTARG"
+	exit
+        ;;
+      ":")
+        echo "No argument value for option $OPTARG"
+	exit
+        ;;
+      *)
+      # Should not occur
+        echo "Unknown error while processing options"
+        ;;
+    esac
+#    echo "OPTIND is now $OPTIND"
+  done
+echo ${sabmap[$JOB]}
 
-while [[ $# -gt 1 ]]
-do
-key="$1"
-case $key in
-    -j|--job)
-    JOB="$2"
-    shift # past argument
-    ;;
-    -h|--help)
-	echo "Specify one of -j|--job [packethandler,watson,datacentersink,wavgen,speechprocessing,all]"
-    echo "Example commands:"
-    echo " ./submitJob.sh -j speechprocessing --numWatsonHosts 3 --watsonEngineCountList [1,2,3] # submit packethandler,watson,and datacentersink"
-    echo " ./submitJob.sh -j packethandler --numWatsonHosts 3 --watsonEngineCountList [1,2,3]"
-	echo " ./submitJob.sh -j watson --numWatsonHosts 3 --watsonEngineCountList [1,2,3]"
-	echo " ./submitJob.sh -j datacentersink "
-	echo " ./submitJob.sh -j wavgen "
-    exit
-    ;;
-    -n|--numWatsonHosts)
-    numWatsonHosts="$2"
-    shift # past argument
-    ;;
-    -w|--watsonEngineCountList)
-    watsonEngineCountList="$2"
-    echo "watsonEngineCountList: "$watsonEngineCountList
-    shift # past argument
-    ;;
-    *)
-	    echo "Unknown option $key. Must specify one of -j|--job [packethandler,watson,datacentersink,wavgen]"
-	    echo "Example commands: ./submitJob.sh -j packethandler --numWatsonHosts 3 --watsonEngineCountList [1,2,3]"
-		echo " ./submitJob.sh -j watson --numWatsonHosts 3 --watsonEngineCountList [1,2,3]"
-		echo "./submitJob.sh -j datacentersink "
-		echo "./submitJob.sh -j wavgen "
-	    exit
-            # unknown option
-    ;;
-esac
-shift # past argument or value
-done
+xbase=${sabmap[$JOB]##*/}
+basename=${xbase%.*}
+namespace=${basename%.*}
 
-if [[ ($JOB == "watson" || $JOB == "packethandler" || $JOB == "speechprocessing" || $JOB == "all") && $watsonEngineCountList == "" ]] \
-	|| [[ $watsonEngineCountList != "" && ($JOB != "watson" && $JOB != "packethandler" && $JOB != "speechprocessing" && $JOB != "all") ]]
-then
-    echo "You must specify the -w|--watsonEngineCountList option with (and only with) the watson/packethandler jobs." 
-    echo "Example command: ./submitJob.sh -j packethandler --numWatsonHosts 3 --watsonEngineCountList [1,2,3]"
-    exit 
-fi
-
-if [[ ($JOB == "watson" || $JOB == "packethandler") && $numWatsonHosts == "" ]] || [[ $numWatsonJobs != "" && ($JOB != "watson" && $JOB != "packethandler") ]]
-then
-    echo "You must specify the -n|--numWatsonHosts option with (and only with) the watson/packethandler jobs." 
-    exit 
-fi
-
-if [[ $JOB == "speechprocessing" || $JOB == "all" ]]
-then
-	array=( "datacentersink" "watson" "packethandler" ) 
+echo $namespace
+if [ -n "${sabmap[$JOB] + 1}" ]; then
+	echo "Deploying this job "$JOB" with this bundle "${sabmap[$JOB]};
 else
-	array=( $JOB )
+	echo "Didn't recognize name "$JOB;
+	exit
 fi
 
-for JOB in "${array[@]}"
-do
-	xbase=${sabmap[$JOB]##*/}
-	basename=${xbase%.*}
-	namespace=${basename%.*}
-	
-	echo $namespace
-	if [ -n "${sabmap[$JOB] + 1}" ]; then
-		echo "Deploying this job "$JOB" with this bundle "${sabmap[$JOB]};
-	else
-		echo "Didn't recognize name "$JOB;
-		exit
-	fi
-	
-	echo "Submitting .sab file: "${sabmap[$JOB]}
-	echo "To Domain: "$STREAMS_DEFAULT_DOMAIN
-	echo "To Instance: "$STREAMS_DEFAULT_IID
-	echo "With job name: "${jobnamemap[$JOB]}  
-	
-	cat  etc/properties.cfg | grep -i $namespace > etc/tempallproperties.cfg
-	params=""
-	JOB_CONFIG_FILE="" # reset in case we don't find one for this job
-	while read p; do
-	        if [[ ${p:0:1} == "c" ]];
-	        then
-			if [[ $p == *"JobConfig"* ]]; then
-				JOB_CONFIG_FILE=${p#*=}
-			elif [[ $p == *"numWatsonHosts"* ]]; then
-				p="${p/XXX/$numWatsonHosts}"
-				params="$params -P $p"
-			elif [[ $p == *"watsonEngineCountList"* ]]; then
-				p="${p/YYY/$watsonEngineCountList}"
-				params="$params -P $p"
-			else
-	            params="$params -P $p"
-			fi
-	#		echo "$params" 
-	#	else
-	#		echo "bad $p"
-	        fi
-	done < etc/tempallproperties.cfg
-	
-	# get an array out of the watsonEngineCountList
-	engineList="${watsonEngineCountList//[}"
-	engineList="${engineList//]}"
-	IFS=', ' read -r -a jobCountArray <<< "$engineList"
-	
-	jobId=0
-	while [[ ($jobId < 1)  || ("$JOB" == "watson" && $jobId < $numWatsonHosts ) ]]
-	do
-		echo ""
-		echo "Submitting "$JOB":"
-		echo ""
-		
-		jobConfig=""
-		if [ ${JOB_CONFIG_FILE} ]; then 
-			# copy it locally so we can modify
-			jobConfigFile=${JOB_CONFIG_FILE}
-			jobConfigFile="${jobConfigFile/XXX/$((jobId+1))}"
-			hostTag=`cat ${jobConfigFile} | grep targetTagSet`
-			hostTag=${hostTag#*:}
-			echo "Expect host tag where this job will be placed is (to modify, change ${jobConfigFile}): "$hostTag
-			jobConfig=" --jobConfig ${jobConfigFile} "
-		fi
-		
-		jobNameArg="--jobname ${jobnamemap[$JOB]}"
-		jobNameArg="${jobNameArg/XXX/$((jobId+1))}"
-		
-		paramsArg=${params}
-		paramsArg="${paramsArg/XXX/$jobId}"
-		paramsArg="${paramsArg/YYY/${jobCountArray[$jobId]}}"
-		
-		cmd="streamtool submitjob $jobNameArg -d $STREAMS_DEFAULT_DOMAIN -i $STREAMS_DEFAULT_IID $paramsArg $jobConfig ${sabmap[$JOB]}"
-		
-		echo $cmd;
-		
-		eval "$cmd"
-		jobId=$((jobId+1))
+echo ${sabmap[$JOB]}
+echo $STREAMS_DEFAULT_DOMAIN
+echo $STREAMS_DEFAULT_IID
+echo $JOB  
+
+cat  etc/properties.cfg | grep -i $namespace > etc/tempallproperties.cfg
+params=""
+while read p; do
+        if [[ ${p:0:1} == "c" ]];
+        then
+                params="$params -P $p"
+#		echo "$params" 
+#	else
+#		echo "bad $p"
+        fi
+done < etc/tempallproperties.cfg
+ 
+if [ "$JOB" = "wavgen2wav" ]; then
+	dirs=`grep "com.ibm.streamsx.speech2text.speechprocessing.wavgen::WavGenerator.moveToDirectory" etc/tempallproperties.cfg | awk -F = '{print $2}' |tr "," "\n"`
+	i=1;
+	for m in $dirs; do 
+		p2=" -P com.ibm.streamsx.speech2text.speechprocessing.wavgen::WavGenerator.payloadDirPath=$m $params"
+		echo $p2
+		streamtool submitjob -d $STREAMS_DEFAULT_DOMAIN -i $STREAMS_DEFAULT_IID --jobname $JOB$i ${sabmap[$JOB]} $p2
+		let i=i+1
 	done
-done
+else
+	echo $params
+	streamtool submitjob -d $STREAMS_DEFAULT_DOMAIN -i $STREAMS_DEFAULT_IID --jobname $JOB ${sabmap[$JOB]} $params
+fi
 
 rm etc/tempallproperties.cfg
