4324f075c77ea5fb2618cfea8ff0798678d08812
Alex-Cook4 <cooka@us.ibm.com>
2017-07-18 12:53:07 +0300

    Update parallelPerf.sh

--- applications/S2TPerformanceTests/parallelPerf.sh
+++ applications/S2TPerformanceTests/parallelPerf.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-echo "./script.sh <test-name> <watsonModel> <watsonConfig> <writeUtterancesToFile (bool)> <blockSize> <numRepetitions> <numFiles> <audioDirectory> <parallelWidth (list)>"
+echo "./script.sh <test-name> <watsonModel> <watsonConfig> <writeUtterancesToFile (bool)> <blockSize> <executionsPerAudioFile> <numAudioFiles> <audioDirectory> <parallelWidth (list)>"
 
 name=$1;
 watsonModelFile=$2;
 watsonConfigFile=$3;
 writeUtterancesToFile=$4;
 blockSize=$5;
-numRepetitions=$6
-numFiles=$7
+executionsPerAudioFile=$6
+numAudioFiles=$7
 audioDirectory=$8
 parallelWidth=$9; # should be list argument '1 3 7 10'
-
-#testName="$name_$width_$blockSize_$numFiles_$numRepetitions_$writeUtterancesToFile"
 
 echo "Width $parallelWidth";
 
 for width in $parallelWidth;
 do
-	testName="${name}_parallel${width}_block${blockSize}_${numFiles}files_${numRepetitions}repetitions_${writeUtterancesToFile}";
-	cmd="streamtool submitjob output/com.ibm.streamsx.speech2text.perf.PerfTest.sab -P watsonModelFile=$watsonModelFile -P watsonConfigFile=$watsonConfigFile -P audioDirectory=$audioDirectory -P numFiles=$numFiles -P numRepetitions=$numRepetitions -P blockSize=$blockSize -P writeUtterancesToFile=$writeUtterancesToFile -P testName=$testName -P parallelWidth=$width --jobname $testName";
+	testName="${name}_parallel${width}_block${blockSize}_${numAudioFiles}files_${executionsPerAudioFile}executionsPerAudioFile${writeUtterancesToFile}";
+	cmd="streamtool submitjob output/com.ibm.streamsx.speech2text.perf.PerfTest.sab -P watsonModelFile=$watsonModelFile -P watsonConfigFile=$watsonConfigFile -P audioDirectory=$audioDirectory -P numAudioFiles=$numAudioFiles -P executionsPerAudioFile=$executionsPerAudioFile -P blockSize=$blockSize -P writeUtterancesToFile=$writeUtterancesToFile -P testName=$testName -P parallelWidth=$width --jobname $testName";
 	echo $cmd;
 	`$cmd`;
 	while ! grep -i "completed" data/${testName}_Performance.txt; do  echo "Waiting for $testName to complete...";  sleep 30; done
