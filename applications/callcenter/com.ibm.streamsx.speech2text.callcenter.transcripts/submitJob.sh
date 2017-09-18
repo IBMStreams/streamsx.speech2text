f92178a520bfa9fe3832b14808aed3b24fc1ae6d
Alex-Cook4 <cooka@us.ibm.com>
2017-09-18 14:48:20 -0400

    Refactoring of Transcripts to allow for generalized
    Pulled out single channel to make it an "adapter" so that people can use
    either single channel, or build their own "multi-channel" adapter.

--- applications/callcenter/com.ibm.streamsx.speech2text.callcenter.transcripts/submitJob.sh
+++ applications/callcenter/com.ibm.streamsx.speech2text.callcenter.transcripts/submitJob.sh
@@ -4,11 +4,33 @@ STREAMS_DEFAULT_DOMAIN=$STREAMS_DOMAIN_ID
 STREAMS_DEFAULT_IID=$STREAMS_INSTANCE_ID
 
 # Modify the SAB file to the one of interest
-SAB_FILE=output/com.ibm.streamsx.speech2text.callcenter.transcripts.singlechannel.SingleChannelTranscriptBuilder/BuildConfig/com.ibm.streamsx.speech2text.callcenter.transcripts.singlechannel.SingleChannelTranscriptBuilder.sab
+TRANSCRIPT_SAB_FILE=output/com.ibm.streamsx.speech2text.callcenter.transcripts/com.ibm.streamsx.speech2text.callcenter.transcripts.TranscriptBuilder.sab
+SINGLECHANNEL_SAB_FILE=output/com.ibm.streamsx.speech2text.callcenter.transcripts/com.ibm.streamsx.speech2text.callcenter.transcripts.singlechannel.SingleChannelTranscriptAdapter.sab
 # Modify the job name to the desired one 
-JOB_NAME=SingleChannelTranscripts
+#JOB_NAME=SingleChannelTranscripts
 # Job config file is used for dynamic tagging
 JOB_CONFIG_FILE="etc/jobConfig.json" 
+
+declare -A sabmap
+sabmap[transcriptbuilder]=$TRANSCRIPT_SAB_FILE
+sabmap[singlechannel]=$SINGLECHANNEL_SAB_FILE
+
+declare -A jobnamemap
+jobnamemap[transcriptbuilder]=TranscriptBuilder
+jobnamemap[singlechannel]=SingleChannelAdapter
+
+if [ "$#" -le 0 ] ; then
+	echo "Must specify one of -j|--job [transcriptbuilder, singlechannel]"
+	echo "Example commands: ./submitJob.sh -j transcriptbuilder "
+	exit
+fi
+
+if [[ $# -lt 2 ]]
+then
+	echo "Must specify one of -j|--job [transcriptbuilder, singlechannel]"
+	echo "Example commands: ./submitJob.sh -j transcriptbuilder "
+    	exit
+fi
 
 
 if [[ $STREAMS_CALLCENTER_DATA == "" ]]
@@ -21,7 +43,31 @@ then
 	fi
 fi
 
+while [[ $# -gt 1 ]]
+do
+key="$1"
+case $key in
+    -j|--job)
+    JOB="$2"
+    shift # past argument
+    ;;
+    -h|--help)
+	echo "Specify one of -j|--job [transcriptbuilder, singlechannel]"
+	echo "Example commands: ./submitJob.sh -j transcriptbuilder "
+    exit
+    ;;
+    *)
+	echo "Unknown option $key. Specify one of -j|--job [transcriptbuilder, singlechannel]"
+	echo "Example commands: ./submitJob.sh -j transcriptbuilder "
+	exit
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
 
+SAB_FILE=${sabmap[$JOB]}
+JOB_NAME=${jobnamemap[$JOB]}
 xbase=${SAB_FILE##*/}
 basename=${xbase%.*}
 namespace=${basename%.*}
