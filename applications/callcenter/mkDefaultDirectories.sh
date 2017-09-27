988ffff2fead0e5504ce587abbe82802eb64e785
Alex-Cook4 <cooka@us.ibm.com>
2017-09-27 12:13:26 -0400

    Remove datacenter sink directory creation
    We don't write to that directory in any applicaiton.

--- applications/callcenter/mkDefaultDirectories.sh
+++ applications/callcenter/mkDefaultDirectories.sh
@@ -1,6 +1,5 @@
 	mkdir -p $STREAMS_CALLCENTER_DATA/packethandler
 	mkdir -p $STREAMS_CALLCENTER_DATA/watson
-	mkdir -p $STREAMS_CALLCENTER_DATA/datacentersink
 	mkdir -p $STREAMS_CALLCENTER_DATA/wavgen/moveToDir
 	mkdir -p $STREAMS_CALLCENTER_DATA/wavgen/writeBin
 	mkdir -p $STREAMS_CALLCENTER_DATA/wavgen/writeWAV
